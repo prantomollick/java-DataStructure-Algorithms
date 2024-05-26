e42382ad637c62d1961fd9a75d2a1a14c231f79a
Pranto Mollick <prantocse01@gmail.com>
2024-05-26 22:07:27 +0600

    Stack implementation

--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
 import datastructure.linkedlist.DoublyLinkedList;
+import datastructure.stack.ArrayStack;
 import datastructure.stack.IntStack;
 
 public class Main {
     
 
     public static void main(String[] args ) {
+        ArrayStack<int[]> stock = new ArrayStack<int[]>();
 
+        int[] arr = {1, 2, 3};
+
+        stock.push(arr);
+
+        System.out.println(stock.peek());
 
     }
 
